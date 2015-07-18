4fe43b13e109a8140f2b3332972e0252087d21c6
m.roppy <roppy@hotmail.co.jp>
2015-07-18 15:13:34 +0900

    BlockLikeMario

---
+++ Assets/MyCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyCamera : MonoBehaviour {
+
+
+	[SerializeField]
+	private Transform player;
+	
+	[SerializeField]
+	private Vector3 offset = new Vector3(0, 0, -5);
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position =player.position + offset;
+	}
+}
